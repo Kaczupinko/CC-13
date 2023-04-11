075580a13b08151e5f64465a3400c6dd6b231fb6
Kaczupinko <119810971+Kaczupinko@users.noreply.github.com>
2023-04-11 03:41:07 +0100

    create calculator with tests

--- calculator.js
+++ calculator.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        throw new Error("Nie można dzielić przez 0");
+        throw new Error("Cannot divide by 0");
     }
     return a / b;
 }
